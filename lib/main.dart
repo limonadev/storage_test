512e53d952109fd8cab583d2ebdaaaf77a314a02
limonadev <limonadev@whiteleaf.dev>
2020-11-12 11:12:51 -0500

    Added load from assets. Added save to storage.

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,173 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Storage Test',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: StoragePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class StoragePage extends StatefulWidget {
+  StoragePage()
+      : storage = FlutterSecureStorage(),
+        storageKey = 'CONTENT';
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  final FlutterSecureStorage storage;
+  final String storageKey;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _StoragePageState createState() => _StoragePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _StoragePageState extends State<StoragePage> {
+  bool checkingStorage = false,
+      loadingFromAssets = false,
+      savingToStorage = false;
+  String content;
+  String message;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            Spacer(),
+            _saveToStorageWidget(),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text('Save to protected SQLite'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Spacer(flex: 2),
+            _assertStorageContentWidget(),
+            Spacer(flex: 2),
+            _loadFromAssetsWidget(),
+            Spacer(),
+            RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+                children: [
+                  TextSpan(
+                    text: 'Messages:\n',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextSpan(text: message ?? ''),
+                ],
+              ),
             ),
+            Spacer(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget _assertStorageContentWidget() {
+    Widget result;
+    if (checkingStorage == true) {
+      result = CircularProgressIndicator();
+    } else {
+      result = OutlinedButton(
+        onPressed: _assertContentFromStorage,
+        child: Text('Assert content from Secure Storage'),
+      );
+    }
+    return result;
+  }
+
+  Widget _loadFromAssetsWidget() {
+    Widget result;
+    if (loadingFromAssets == true) {
+      result = CircularProgressIndicator();
+    } else {
+      result = content == null
+          ? OutlinedButton(
+              onPressed: _loadFileFromAssets,
+              child: Text('Load from assets'),
+            )
+          : Text(
+              'Content from assets is already in RAM',
+            );
+    }
+    return result;
+  }
+
+  Widget _saveToStorageWidget() {
+    Widget result;
+    if (savingToStorage == true) {
+      result = CircularProgressIndicator();
+    } else {
+      result = OutlinedButton(
+        onPressed: _saveContentOnStorage,
+        child: Text('Save to Secure Storage'),
+      );
+    }
+    return result;
+  }
+
+  void _assertContentFromStorage() async {
+    setState(() {
+      checkingStorage = true;
+    });
+
+    var contentFromStorage = await widget.storage.read(
+      key: widget.storageKey,
+    );
+
+    setState(() {
+      message =
+          'Content from file is ${content == contentFromStorage ? 'equal to' : 'different from'} the content in Storage';
+      checkingStorage = false;
+    });
+  }
+
+  void _loadFileFromAssets() async {
+    setState(() {
+      loadingFromAssets = true;
+    });
+
+    var start = DateTime.now();
+    content = await rootBundle.loadString('assets/files/random_words.txt');
+    var end = DateTime.now();
+
+    setState(() {
+      message =
+          'Loading total time: ${end.difference(start).inMilliseconds} ms';
+      loadingFromAssets = false;
+    });
+  }
+
+  void _saveContentOnStorage() async {
+    setState(() {
+      savingToStorage = true;
+    });
+
+    var start = DateTime.now();
+    await widget.storage.write(key: widget.storageKey, value: content);
+    var end = DateTime.now();
+
+    setState(() {
+      message =
+          'Saving to Storage total time: ${end.difference(start).inMilliseconds} ms';
+      savingToStorage = false;
+    });
   }
 }
